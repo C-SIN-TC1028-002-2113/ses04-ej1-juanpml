7f0ce83dadbe3a684d69ca4f2cec1fc0e4b53d17
juanpml <A01742169@itesm.mx>
2021-08-20 22:19:04 +0000

    Actividad Licencia

--- assignments/02Licencia/src/exercise.py
+++ assignments/02Licencia/src/exercise.py
@@ -1,6 +1,17 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    edad = int(input("Ingresa tu edad: "))
+    if edad < 18:
+        print ("No cumples con los requisitos")
+    else:
+        a = input("¿Tienes identificación oficial? (s/n): ")
+        if a == "s":
+         print ("Trámite de licencia concedido")
+        elif a == "n":
+         print ("No cumples requisitos")
+        else:
+             print ("Respuesta Incorrecta")
+        
 
 if __name__ == '__main__':
     main()
